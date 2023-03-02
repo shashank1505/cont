5e77e5a0c51c94450a70ce91ff84dc918b059a46
shashank potluri <potlurishashank05@gmail.com>
2023-03-02 14:41:12 -0700

    changed numbers

--- addprod.test.js
+++ addprod.test.js
@@ -1,9 +1,9 @@
 const {add,prod}=require('./addprod');
 
 test("testing add 1+2 equalto 3 ",()=>{
-    expect(add(1, 2)).toBe(3);
+    expect(add(2, 1)).toBe(3);
 })
 
 test("testing prod 1*2 equal to 2 ",()=>{
-    expect(prod(1, 2)).toBe(2);
+    expect(prod(5, 2)).toBe(10);
 })
